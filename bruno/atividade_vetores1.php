5dfe51b6bde54a3d9781ce8a61dd6bc3b97a2bba
brunobecksantor <bruno.76050@aluno.iffar.edu.br>
2026-03-04 11:06:04 -0300

    Delete bruno directory

--- bruno/atividade_vetores1.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$vetor1 = ["banana", "morango", "abacate", "manga", "melancia"];
-echo"$vetor1[0] <br> $vetor1[1] <br> $vetor1[2] <br> $vetor1[3] <br> $vetor1[4] <br> ";
-$vetor1 [5] = "laranja";
-echo"$vetor1[5]"
-?>
