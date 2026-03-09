b39f74d9d32746b8dcee49e9cd48c1d98f237b6c
brunobecksantor <bruno.76050@aluno.iffar.edu.br>
2026-03-09 16:55:42 -0300

    Add files via upload

---
+++ agenda30/index.php
@@ -0,0 +1,38 @@
+<html>
+
+    <head>
+
+        <title>Agenda de contatos - T30</title>
+
+    </head>
+
+    <body>
+
+        <h1>Agenta - Turma 30 - 2026</h1>
+        <h2>Cadastrar contatos</h2>
+        <form action="salvar.php" method="post">
+            Nome: <input type="text" name="nome"> <br><br>
+            Endereço: <input type="text" name="endereco"> <br><br>
+            Telefone: <input type="text" name="telefone"> <br><br>
+            <input type="submit" value="Cadastrar">
+        </form>
+
+    <?php 
+    include ('conexao.php');
+    
+    $sql = "SELECT * FROM contatos";
+    $resultado = mysqli_query($conexao, $sql);
+
+    if (mysqli_num_rows($resultado) > 0){
+        while ($linha = mysqli_fetch_assoc($resultado)){
+            echo $linha['nome']  ." | ". $linha['endereco'] ." | ". $linha['telefone'] . "<br>";
+        }
+    } else{
+        echo"<h3>Nenhum contato registrado.</h3>";
+    }
+    
+    ?>
+
+    </body>
+
+</html>
